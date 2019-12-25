eee34c2f0de555a3b31f7d95f11b7b3ab894614b
Talha Abbasi <talhaabbasi1997@gmail.com>
2019-12-26 01:47:01 +0500

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("../sw_cached_pages.js")
+      .then(reg => console.log("Service worker: Registered"))
+      .catch(err => console.log("Service Worker: Error: ${err}"));
+  });
+}
